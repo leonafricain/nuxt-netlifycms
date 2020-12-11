74f6b88d24a59de9c19d4095c85eb8230735b8bf
leonafricain <kimleonafricain@gmail.com>
2020-12-11 23:20:11 +0100

    main change

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="main text-center flex flex-col items-center justify-center">
     <!-- <logo /> -->
     <h1 class="title">Nuxt — Tailwind — Netlify CMS</h1>
-    <h2 class="subtitle">Le site</h2>
+    <h2 class="subtitle">Le site qui me taquine</h2>
   </div>
 </template>
 
